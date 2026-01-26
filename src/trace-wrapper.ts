9a2d624e72b1a857ec72232c8310a701ea3bf5a7
Cole Gawin <colegawin@gmail.com>
2026-01-26 15:10:10 -0800

    feat: Add initial state parameter to `wrapAgent` and record it in the tracing span.

--- src/trace-wrapper.ts
+++ src/trace-wrapper.ts
@@ -1,13 +1,14 @@
 import { context, trace } from "@opentelemetry/api";
 import { v4 as uuidv4 } from "uuid";
 
-export async function wrapAgent<A extends unknown[], F extends (...args: A) => ReturnType<F>>(agentName: string, fn: F, ...args: A) {
+export async function wrapAgent<A extends unknown[], F extends (...args: A) => ReturnType<F>>(agentName: string, initialState: any, fn: F, ...args: A) {
   const tracer = trace.getTracer("lemma");
 
   const runId = uuidv4();
   const span = tracer.startSpan(agentName, {
     attributes: {
       "lemma.agent.run_id": runId,
+      "lemma.agent.input": initialState,
     },
   });
 
