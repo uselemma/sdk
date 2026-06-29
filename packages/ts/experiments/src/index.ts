e01eacfbd226fc4fc8285cbd5cb93be289918c34
Cole Gawin <colegawin@gmail.com>
2026-06-29 14:10:50 -0700

    Remove experiment packages

--- packages/ts/experiments/src/index.ts
+++
@@ -1,9 +0,0 @@
-export { LemmaExperimentRunner } from "./runner";
-export { LemmaExperimentsClient } from "./client";
-export type {
-  ExperimentResult,
-  ExperimentSummary,
-  LemmaExperimentRunnerOptions,
-  RunExperimentOptions,
-  TestCase,
-} from "./types";
